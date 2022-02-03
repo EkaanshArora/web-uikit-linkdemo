42354f2c25285995844bed0f5734ed297c165837
EkaanshArora <ekaansh68@gmail.com>
2022-02-03 19:37:19 +0530

    add links

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,48 @@ import 'agora-react-uikit/dist/index.css'
 
 const App: React.FunctionComponent = () => {
   const [videocall, setVideocall] = useState(true)
-  const [isHost, setHost] = useState(true)
-  const [isPinned, setPinned] = useState(false)
+  const [link, setLink] = useState('')
+
+  const parseParams = () => {
+    const params = new URLSearchParams(window.location.search);
+    const object: any = {};
+    params.forEach((value, key) => {
+      object[key] = value;
+    })
+    console.log('params', object)
+    return object;
+  };
+
+  const createLink = () => {
+    const ts = new Date().getTime()
+    let link = window.location.origin + '/?channel=' + ts
+    console.log(link)
+    setLink(link)
+    return link
+  }
 
   return (
     <div style={styles.container}>
       <div style={styles.videoContainer}>
-        <h1 style={styles.heading}>Agora React Web UI Kit</h1>
+        <h1 style={styles.heading}>Agora React Web UIKit</h1>
         {videocall ? (<>
-          <div style={styles.nav}>
-            <p style={{ fontSize: 20, width: 200 }}>You're {isHost ? 'a host' : 'an audience'}</p>
-            <p style={styles.btn} onClick={() => setHost(!isHost)}>Change Role</p>
-            <p style={styles.btn} onClick={() => setPinned(!isPinned)}>Change Layout</p>
-          </div>
           <AgoraUIKit
             rtcProps={{
-              appId: '<Your Agora App ID>',
+              appId: '',
               channel: 'test',
-              token: null, //add your token if using app in secured mode
-              role: isHost ? 'host' : 'audience',
-              layout: isPinned ? layout.pin : layout.grid
+              token: null,
+              role: 'host',
+              layout: layout.grid,
+              ...parseParams()
             }}
             callbacks={{
               EndCall: () => setVideocall(false),
             }} /></>
         ) : (
           <div style={styles.nav}>
-            <h3 style={styles.btn} onClick={() => setVideocall(true)}>Start Call</h3>
+            <div style={styles.btn} onClick={() => setVideocall(true)}>Start Call</div>
+            <div style={styles.btn} onClick={() => createLink()}>Create Link</div>
+            <div onClick={()=>window.open(link, '_blank')}>{link}</div>
           </div>
         )}
       </div>
@@ -39,7 +54,7 @@ const App: React.FunctionComponent = () => {
 }
 
 const styles = {
-  container: { width: '100vw', height: '100vh', display: 'flex', flex: 1, backgroundColor: '#007bff22'},
+  container: { width: '100vw', height: '100vh', display: 'flex', flex: 1},
   heading: { textAlign: 'center' as const, marginBottom: 0 },
   videoContainer: { display: 'flex', flexDirection: 'column', flex: 1 } as React.CSSProperties,
   nav: { display: 'flex', justifyContent: 'space-around' },
